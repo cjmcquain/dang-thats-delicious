8d922eda515fa43994e128ff15d6438469c94190
Charles McQuain <charlesmcquain@gmail.com>
2017-12-06 23:11:58 -0800

    Added Tags Route & Tags Page Template

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -73,4 +73,10 @@ exports.getStoreBySlug = async (req, res) => {
     const store = await Store.findOne({ slug: req.params.slug });
     if (!store) return next();
     res.render('store', { store, title: store.name});
-}
+};
+
+exports.getStoresByTag = async (req, res) => {
+    const tags = await Store.getTagsList();
+    const tag = req.params.tag;
+    res.render('tag', { tags, title: 'Tags', tag });
+  };
